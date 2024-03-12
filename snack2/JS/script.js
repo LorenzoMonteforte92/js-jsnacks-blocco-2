9df1b71546d5bbcecfc577ab58ce1595f61ea079
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-03-12 17:54:48 +0100

    feat: add user prompt

--- snack2/JS/script.js
+++ snack2/JS/script.js
@@ -1,2 +1,9 @@
 // L’utente inserisce due parole in successione, con due prompt.
 // Il software stampa prima la parola più corta, poi la parola più lunga
+
+//chiedere all'utente le due parole
+userWordOne = prompt(`inserisci la prima parola`);
+userWordTwo = prompt(`inserisci la seconda parola`);
+
+console.log(userWordOne);
+console.log(userWordTwo);
