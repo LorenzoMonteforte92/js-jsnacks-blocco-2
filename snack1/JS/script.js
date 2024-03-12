92f00811693ba0748e28fce258dacb960c37e751
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-03-12 17:33:57 +0100

    feat: add user prompt

---
+++ snack1/JS/script.js
@@ -0,0 +1,6 @@
+// L'utente inserisce un numero nel prompt, se è pari stampa il numero,
+// se è dispari stampa il numero successivo
+
+// chiedere numero all'utente
+userNumber = parseInt(prompt(`Inserisci un numero`));
+console.log(userNumber)
